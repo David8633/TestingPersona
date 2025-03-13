083722be6f62fa98ff2b49cba89e6862a06f8812
David8633 <davidinstituto282@gmail.com>
2025-03-13 10:36:06 +0100

    Añadido el test de es mayor de edad donde si metes el valor erroneo salta la exception

--- persona/src/persona/testing.java
+++ persona/src/persona/testing.java
@@ -1,17 +1,22 @@
 package personaTest;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 import persona.Persona;
+import persona.invalidadEdadException;
 
 class testing {
 
 	private static Persona p1 = new Persona();;
 	private static Persona p2 = new Persona("Pepe",24,'H');
 	private static Persona p3 = new Persona("pili",17,'H');
+	private static Persona p4 = new Persona("Koi",-2,'M');
 	
 
 
@@ -24,4 +29,15 @@ class testing {
 		
 	}
 
+	
+	@Test
+	void testEsMayorEdad() throws invalidadEdadException {
+		assertFalse(p3.esMayorDeEdad());
+		//assertFalse(p1.esMayorDeEdad());
+		assertTrue(p2.esMayorDeEdad());
+		assertThrows("la edad introducidad es unda edad invalida",p4.esMayorDeEdad());
+		
+	}
+	
+	
 }
