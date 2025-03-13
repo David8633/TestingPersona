b88da60bda9b7413f3ca1ad1dfd826365057c28d
David8633 <davidinstituto282@gmail.com>
2025-03-13 21:14:27 +0100

    añadido el test de la creaccion de contructor con las dos excepciones

--- persona/src/persona/testing.java
+++ persona/src/persona/testing.java
@@ -7,22 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import persona.Persona;
 import persona.invalidadEdadException;
-
-class testing {
-
-	private static Persona p1 = new Persona();;
-	private static Persona p2 = new Persona("Pepe",24,'H');
-	private static Persona p3 = new Persona("pili",17,'H');
-	private static Persona p4 = new Persona("Koi",-2,'M');
-	private static Persona p5 = new Persona("Luca",24,'H',120,1.5);
-	private static Persona p6 = new Persona("Andi",17,'H',40,1.55);
-	private static Persona p7 = new Persona("Junajo",25,'M',78,1.85);
+import persona.pesoInvalidoException;
 
 
+
+	class testing { 
+		
+			private static Persona p1 = new Persona();;
+			private static Persona p2 = new Persona("Pepe",24,'H');
+			private static Persona p3 = new Persona("pili",17,'H');
+			private static Persona p4 = new Persona("Koi",-2,'M');
+			private static Persona p5 = new Persona("Luca",24,'H',120,1.5);
+			private static Persona p6 = new Persona("Andi",17,'H',40,1.55);
+			private static Persona p7 = new Persona("Juanjo",25,'M',78,1.85);
+			private static Persona p8 = new Persona("Andi",17,'H',9,1.55);
+			private static Persona p9 = new Persona("Lucius",20,'M',78,0.90);
+			private static Persona p10 = new Persona("Harry",17,'H',9,1.55);
+
+		
 	@Test
 	void testNombre() {
 		p3.setNombre("Javier");
@@ -42,6 +49,11 @@ class testing {
 		
 	}
 	
+	@Test
+	void testCreaccionConstructor() {
+		assertThrows("el peso introducido no es valido",p9);
+		assertThrows("La altura que has introducido no es valida",p10);
+	}
 	
 	
 	@Test
